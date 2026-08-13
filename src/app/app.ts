68f157f87c6a72a4a4feb6e5b48be52e9a0f9567
Josue Mejia <josueomejiap@gmail.com>
2026-08-12 18:45:22 -0600

    clase 100826

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('primera_app');
+  //protected readonly title = signal('primera_app');
+  titulo = "app de Ricardo";
 }
